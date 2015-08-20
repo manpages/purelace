3ba90d532e66132dedcecbd94fd7b01c733b1d32
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-08-20 22:36:40 +0200

    Get to Document type.

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,15 @@
 module Main where
 
+import Prelude
 import Control.Monad.Eff.Console
+import DOM.HTML
+import DOM.HTML.Types
+import DOM.HTML.Window
+import DOM.Node.Document
 
 main = do
-  log "Hello sailor!"
+  w  <- window
+  hd <- document w
+  let d  = htmlDocumentToDocument hd
+  u  <- url d
+  log $ "Wow! " ++ show u
