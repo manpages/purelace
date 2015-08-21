99dd89ef67f73ee55b4b90cc667a0cd18edee1a6
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-08-21 02:05:09 +0200

    Add type signature

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,15 @@
 module Main where
 
 import Prelude
+import Control.Monad.Eff
 import Control.Monad.Eff.Console
+import DOM
 import DOM.HTML
 import DOM.HTML.Types
 import DOM.HTML.Window
 import DOM.Node.Document
 
+main :: forall eff. Eff (console :: CONSOLE, dom :: DOM | eff) Unit
 main = do
   w  <- window
   hd <- document w
