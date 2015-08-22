10946bbaa172b8627a733fd34923eac25f92fe87
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-08-22 03:26:38 +0200

    Implement DOM.Node.Attr

--- src/Main.purs
+++ src/Main.purs
@@ -9,12 +9,15 @@ import DOM.HTML.Types
 import DOM.HTML.Window
 import DOM.HTML.Document
 import DOM.Node.Types
+import qualified DOM.Node.Document as ND
 import DOM.Node.Element hiding (id)
 import qualified DOM.Node.HTMLCollection as C
 import Data.Nullable (Nullable(), toMaybe)
 import Data.Maybe (Maybe(..))
 import Data.Maybe.Unsafe (fromJust)
 import qualified DOM.Node.Node as N
+import qualified Unsafe.Coerce as U
+import DOM.Node.Attr
 
 cardImgURL :: String -> String
 cardImgURL "Purelace"       = "http://magiccards.info/scans/en/4e/293.jpg"
@@ -29,20 +32,42 @@ coerceE = htmlElementToElement
 coerceD :: HTMLDocument -> Document
 coerceD = htmlDocumentToDocument
 
+e2n :: Element -> Node
+e2n = elementToNode
+
+n2e :: Element -> Node
+n2e = U.unsafeCoerce
+
+htmlDoc :: forall eff. Eff (dom :: DOM | eff) HTMLDocument
+htmlDoc = do
+  w <- window
+  document w
+
+doc' :: forall eff. Eff (dom :: DOM | eff) Document
+doc' = do
+  d <- htmlDoc
+  return $ coerceD d
+
 htmlBody :: forall eff. Eff (dom :: DOM | eff) (Nullable HTMLElement)
 htmlBody = do
   w <- window
   d <- document w
   body d
 
+body' :: forall eff. Eff (dom :: DOM | eff) Element
+body' = do
+  hb <- htmlBody
+  return $ coerceE $ unsafeA hb
+
 unsafeA :: forall a. (Nullable a) -> a
 unsafeA = fromJust <<< toMaybe
 
 main :: forall eff. Eff (console :: CONSOLE, dom :: DOM | eff) Unit
 main = do
-  hb <- htmlBody
-  let b = coerceE $ unsafeA $ hb
-  hes  <- getElementsByClassName "card" b
-  one  <- C.item 1 hes
-  v    <- N.textContent $ elementToNode $ unsafeA $ one
-  log $ "Wow! " ++ show v
+  d <- doc'
+  b <- body'
+  img  <- ND.createElement "img" d
+  _    <- setAttribute "src" (cardImgURL "Chill") img
+  b1   <- N.appendChild (e2n img) (e2n b)
+  src  <- getAttribute "src" img
+  log $ show $ unsafeA src == cardImgURL "Chill"
